f34c15c0a57ddf7080c3bc39553b6fdf70a1702c
Mayur chaudhary <52111254+hacmak18@users.noreply.github.com>
2019-07-03 22:20:50 +0530

    Add files via upload

---
+++ App_Code/datalayer.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+
+public class datalayer
+{
+    SqlConnection cn;
+    SqlCommand cmd;
+    SqlDataAdapter da;
+    DataSet ds;
+    DataTable dt;
+
+    public datalayer()
+    {
+        cn = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+
+    }
+
+    public DataSet state_query()
+    {
+        da = new SqlDataAdapter("select * from state_table", cn);
+        ds = new DataSet();
+        da.Fill(ds);
+        return ds;
+    }
+    public DataSet city_query(logiclayer li)
+    {
+        cmd = new SqlCommand("select * from city_table where state_id=@sid", cn);
+        cmd.Parameters.AddWithValue("@sid", li.state_id);
+
+        da = new SqlDataAdapter(cmd);
+        ds = new DataSet();
+        da.Fill(ds);
+
+        return ds;
+
+    }
+
+    public void insert_query(logiclayer li)
+    {
+        cmd = new SqlCommand(@"INSERT INTO tolltax.dbo.User_table(F_name,L_name,Mob_no,Email_id,Address,City_name,Pin_code,[User_name],Password) 
+                                                        VALUES (@fn,@ln,@mob,@em,@add,@ct,@pin,@un,@pass)", cn);
+
+        cmd.Parameters.AddWithValue("@fn", li.fname);
+        cmd.Parameters.AddWithValue("@ln", li.lname);
+        cmd.Parameters.AddWithValue("@em", li.email);
+        cmd.Parameters.AddWithValue("@un", li.uname);
+        cmd.Parameters.AddWithValue("@pass", li.pass);
+        cmd.Parameters.AddWithValue("@add", li.address);
+        cmd.Parameters.AddWithValue("@ct", li.dcity);
+        cmd.Parameters.AddWithValue("@mob", li.mob_no);
+        cmd.Parameters.AddWithValue("@pin", li.pc);
+
+
+
+
+
+        cn.Open();
+        cmd.ExecuteNonQuery();
+        cn.Close();
+    }
+    public DataTable admin_login(logiclayer li)
+    {
+        SqlCommand cmd = new SqlCommand("select * from Admin_login where  User_Name =@Ausername and admin_Password=@password", cn);
+        cmd.Parameters.AddWithValue("@Ausername", li.user_name);
+        cmd.Parameters.AddWithValue("@password", li.admin_password);
+        da = new SqlDataAdapter(cmd);
+        dt = new DataTable();
+        da.Fill(dt);
+        return dt;
+    }
+    public DataTable operator_login(logiclayer li)
+    {
+        SqlCommand cmd = new SqlCommand("select * from operator_table where  Uplace_name =@Ausername and Password=@password", cn);
+        cmd.Parameters.AddWithValue("@Ausername", li.user_name);
+        cmd.Parameters.AddWithValue("@password", li.admin_password);
+        da = new SqlDataAdapter(cmd);
+        dt = new DataTable();
+        da.Fill(dt);
+        return dt;
+    }
+    public void insert_contact(logiclayer li)
+    {
+        cmd = new SqlCommand(@"INSERT INTO tolltax.dbo.contact(Username,Email,Message) VALUES(@UN,@mail,@msg)", cn);
+
+        cmd.Parameters.AddWithValue("@UN", li.cntct_user1);
+        cmd.Parameters.AddWithValue("@mail", li.cntct_email1);
+        cmd.Parameters.AddWithValue("@msg", li.cntct_msg);
+
+
+        cn.Open();
+        cmd.ExecuteNonQuery();
+        cn.Close();
+    }
+    public DataTable user_login(logiclayer li)
+    {
+        SqlCommand cmd = new SqlCommand("select * from user_table where  user_name =@username and Password=@user_password", cn);
+        cmd.Parameters.AddWithValue("@username", li.username);
+        cmd.Parameters.AddWithValue("@user_password", li.password);
+        da = new SqlDataAdapter(cmd);
+        dt = new DataTable();
+        da.Fill(dt);
+        return dt;
+    }
+    public void insert_feedback(logiclayer li)
+    {
+        cmd = new SqlCommand(@"INSERT INTO feedback(fb_uname,fb_mail,fb_help,fb_review) 
+                                                        VALUES (@fbname,@fbmail,@fbhelp,@fbreview)", cn);
+
+        cmd.Parameters.AddWithValue("@fbname", li.fb_uname);
+        cmd.Parameters.AddWithValue("@fbmail", li.fb_mail);
+        cmd.Parameters.AddWithValue("@fbhelp", li.fb_help);
+        cmd.Parameters.AddWithValue("@fbreview", li.fb_review);
+
+
+
+        cn.Open();
+        cmd.ExecuteNonQuery();
+        cn.Close();
+    }
+    public DataTable view(logiclayer li)
+    {
+        cmd = new SqlCommand("select * from User_table", cn);
+        da = new SqlDataAdapter(cmd);
+        dt = new DataTable();
+        da.Fill(dt);
+        return dt;
+
+    }
+    public DataTable contact_view(logiclayer li)
+    {
+        cmd = new SqlCommand("select * from contact", cn);
+        da = new SqlDataAdapter(cmd);
+        dt = new DataTable();
+        da.Fill(dt);
+        return dt;
+    }
+    public DataTable fb_view(logiclayer li)
+    {
+        cmd = new SqlCommand("select * from feedback", cn);
+        da = new SqlDataAdapter(cmd);
+        dt = new DataTable();
+        da.Fill(dt);
+        return dt;
+    }
+    public DataTable toll_view(logiclayer li)
+    {
+        cmd = new SqlCommand("select * from toll_trans", cn);
+        da = new SqlDataAdapter(cmd);
+        dt = new DataTable();
+        da.Fill(dt);
+        return dt;
+    }
+    public DataTable t_profile(logiclayer li)
+    {
+        cmd = new SqlCommand("select * from toll_trans where card_no=@cd ", cn);
+        cmd.Parameters.AddWithValue("@cd", li.ptcn);
+        da = new SqlDataAdapter(cmd);
+        dt = new DataTable();
+        da.Fill(dt);
+        return dt;
+    }
+    public DataTable trans_view(logiclayer li)
+    {
+        cmd = new SqlCommand("select * from transaction_table", cn);
+        da = new SqlDataAdapter(cmd);
+        dt = new DataTable();
+        da.Fill(dt);
+        return dt;
+    }
+
+    public DataTable selelectname(logiclayer li)
+    {
+        cmd = new SqlCommand("select * from user_table where card_no =@tp", cn);
+        cmd.Parameters.AddWithValue("@tp", li.ccd);
+        da = new SqlDataAdapter(cmd);
+        dt = new DataTable();
+        da.Fill(dt);
+        return dt;
+    }
+    public DataTable city_view(logiclayer li)
+    {
+        cmd = new SqlCommand(" select * from City_table c inner join State_table s on s.State_id=c.State_id", cn);
+        da = new SqlDataAdapter(cmd);
+        dt = new DataTable();
+        da.Fill(dt);
+        return dt;
+    }
+    public void insert_recharge(logiclayer li)
+    {
+
+        cmd = new SqlCommand(@"INSERT INTO tolltax.dbo.transaction_table(user_name,card_no,trans_date,Balance,trans_type) VALUES(@un,@cn,getdate(),@ta,@tt)", cn);
+
+        cmd.Parameters.AddWithValue("@un", li.t_uname);
+        cmd.Parameters.AddWithValue("@cn", li.t_crdno);
+        cmd.Parameters.AddWithValue("@ta", li.t_amount);
+        cmd.Parameters.AddWithValue("@tt", li.t_type);
+        cn.Open();
+        cmd.ExecuteNonQuery();
+        cn.Close();
+    }
+    public void ins_add(logiclayer li)
+    {
+        cmd = new SqlCommand("update user_table set balance=(balance+@amount) where card_no=@crdno", cn);
+        cmd.Parameters.AddWithValue("@crdno", li.t_crdno);
+        cmd.Parameters.AddWithValue("@amount", li.t_amount);
+
+        cn.Open();
+        cmd.ExecuteNonQuery();
+        cn.Close();
+
+    }
+    public void truck(logiclayer li)
+    {
+        cmd = new SqlCommand("update user_table set balance=(balance-100) where card_no=@crdno", cn);
+        cmd.Parameters.AddWithValue("@crdno", li.tcd);
+
+
+        cn.Open();
+        cmd.ExecuteNonQuery();
+        cn.Close();
+    }
+    public void car(logiclayer li)
+    {
+        cmd = new SqlCommand("update user_table set balance=(balance-50) where card_no=@crdno", cn);
+        cmd.Parameters.AddWithValue("@crdno", li.ccd);
+
+
+        cn.Open();
+        cmd.ExecuteNonQuery();
+        cn.Close();
+    }
+    public void insert_toll(logiclayer li)
+    {
+
+        cmd = new SqlCommand(@"INSERT INTO tolltax.dbo.toll_trans(card_no,amount,type,date) VALUES(@cn,@amount,@type,getdate())", cn);
+
+        cmd.Parameters.AddWithValue("@cn", li.crd);
+        cmd.Parameters.AddWithValue("@amount", li.amount);
+        cmd.Parameters.AddWithValue("@type", li.type);
+        cn.Open();
+        cmd.ExecuteNonQuery();
+        cn.Close();
+    }
+    public DataTable databse(logiclayer li)
+    {
+        cmd = new SqlCommand("select * from user_table where user_name=@Eid", cn);
+        cmd.Parameters.AddWithValue("@Eid", li.Eid);
+
+        da = new SqlDataAdapter(cmd);
+        dt = new DataTable();
+        da.Fill(dt);
+        return dt;
+    }
+    public void insertdb(logiclayer li)
+    {
+        cmd = new SqlCommand("update user_table set card_no=@crd_no where User_name=@uname", cn);
+        cmd.Parameters.AddWithValue("@crd_no", li.iddb_crd);
+        cmd.Parameters.AddWithValue("@uname", li.cduname);
+        cn.Open();
+        cmd.ExecuteNonQuery();
+        cn.Close();
+    }
+    public DataTable get(logiclayer li)
+    {
+        cmd = new SqlCommand("select * from User_table where user_name=@guname", cn);
+         cmd.Parameters.AddWithValue("@guname",li.guname);
+
+         da = new SqlDataAdapter(cmd);
+         dt = new DataTable();
+         da.Fill(dt);
+         return dt;
+
+    }
+    public DataTable rcg(logiclayer li)
+    {
+        cmd = new SqlCommand("select * from user_table where card_no=@cdno", cn);
+        cmd.Parameters.AddWithValue("@cdno", li.rcg);
+
+        da = new SqlDataAdapter(cmd);
+        dt = new DataTable();
+        da.Fill(dt);
+        return dt;
+    }
+}
---
+++ App_Code/logiclayer.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+public class logiclayer
+{
+    datalayer dl = new datalayer();
+
+    public int state_id { get; set; }
+    public string fname { get; set; }
+    public string lname { get; set; }
+    public string uname { get; set; }
+
+    public string email { get; set; }
+    public string pass { get; set; }
+    public string address { get; set; }
+    public string dstat { get; set; }
+    public string dcity { get; set; }
+    public decimal pc { get; set; }
+    public string mob_no { get; set; }
+    public string user_name { get; set; }
+    public string admin_password { get; set; }
+    public string cntct_user1 { get; set; }
+    public string cntct_email1 { get; set; }
+    public string cntct_msg { get; set; }
+    public string username { get; set; }
+    public string password { get; set; }
+    public string fb_uname { get; set; }
+    public string fb_mail { get; set; }
+    public string fb_review { get; set; }
+    public string fb_help { get; set; }
+    public string card_id { get; set; }
+    public string t_amount { get; set; }
+    public string t_type { get; set; }
+    public string t_uname { get; set; }
+    public string t_crdno { get; set; }
+    public string ccd { get; set; }
+    public string tcd { get; set; }
+    public string crd { get; set; }
+    public string amount { get; set; }
+    public string type { get; set; }
+    public string Eid { get; set; }
+    public string iddb_crd { get; set; }
+    public string cduname { get; set; }
+    public string guname { get; set; }
+    public string gf_name { get; set; }
+    public string gl_name { get; set; }
+    public string g_cn { get; set; }
+    public string g_email { get; set; }
+    public string g_address { get; set; }
+    public string g_mobile { get; set; }
+    public string tp_uname { get; set; }
+    public string ptcn { get; set; }
+    public string rcg { get; set; }
+   
+
+	public logiclayer()
+	{
+
+    }
+
+   public DataTable a_login(logiclayer li)
+    {
+        return dl.admin_login(li);
+    }
+    public DataSet getstate()
+    {
+        return dl.state_query();
+    }
+
+    public DataSet getcitiesbyname(logiclayer li)
+    {
+        return dl.city_query(li);
+    }
+
+    public void Reg_User(logiclayer li)
+    {
+        dl.insert_query(li);
+    }
+    public void insert_contact(logiclayer li)
+    {
+        dl.insert_contact(li);
+    }
+    public DataTable u_login(logiclayer li)
+    {
+        return dl.user_login(li);
+    }
+   public void ins_feedback(logiclayer li)
+    {
+        dl.insert_feedback(li);
+    }
+   public DataTable view(logiclayer li)
+   {
+       return dl.view(li);
+   }
+   public DataTable cview(logiclayer li)
+   {
+       return dl.contact_view(li);
+   }
+   public DataTable fbview(logiclayer li)
+   {
+       return dl.fb_view(li);
+   }
+    public DataTable toll(logiclayer li)
+   {
+       return dl.toll_view(li);
+   }
+    public DataTable trans(logiclayer li)
+    {
+        return dl.trans_view(li);
+    }
+    public DataTable tprofile(logiclayer li)
+    {
+        return dl.t_profile(li);
+    }
+    public DataTable cityview(logiclayer li)
+   {
+       return dl.city_view(li);
+   }
+    public void ins_recharge(logiclayer li)
+    {
+        dl.insert_recharge(li);
+    }
+    public void add(logiclayer li)
+    {
+        dl.ins_add(li);
+    }
+    public DataTable sss(logiclayer li)
+    {
+
+      return  dl.selelectname(li);
+    }
+    public void truck_ddct(logiclayer li)
+    {
+        dl.truck(li);
+    }
+    public void car_ddct(logiclayer li)
+    {
+        dl.car(li);
+    }
+    public void ins_db(logiclayer li)
+    {
+        dl.insert_toll(li);
+    }
+    public DataTable o_login(logiclayer li)
+    {
+     return dl.operator_login(li);
+    }
+    public DataTable database(logiclayer li)
+    {
+        return dl.databse(li);
+    }
+    public void insdb(logiclayer li)
+    {
+        dl.insertdb(li);
+    }
+    public DataTable pview(logiclayer li)
+    {
+        return dl.get(li);
+    }
+    public DataTable archg(logiclayer li)
+    {
+        return dl.rcg(li);
+    }
+
+}
